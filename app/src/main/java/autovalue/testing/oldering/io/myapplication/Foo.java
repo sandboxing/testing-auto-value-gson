c70f474381a6dca58b45c58a1f62615bc870644c
benoit <oldering@gmail.com>
2016-09-25 21:57:05 +0900

    setting GsonTypeAdapter inside Foo

--- app/src/main/java/autovalue/testing/oldering/io/myapplication/Foo.java
+++ app/src/main/java/autovalue/testing/oldering/io/myapplication/Foo.java
@@ -1,12 +1,14 @@
 package autovalue.testing.oldering.io.myapplication;
 
 import com.google.auto.value.AutoValue;
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
 
 @AutoValue
 public abstract class Foo {
     abstract String bar();
 
-//    public static TypeAdapter<Foo> typeAdapter(Gson gson) {
-//        return new AutoValue_Foo.GsonTypeAdapter(gson);
-//    }
+    public static TypeAdapter<Foo> typeAdapter(Gson gson) {
+        return new AutoValue_Foo.GsonTypeAdapter(gson);
+    }
 }
